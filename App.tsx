8e96a832f6c19f8b8de33842bfbb77496f21e9ee
Jenish Mariyajerald <jenish.mariyajerald@192.168.1.3>
2023-06-21 12:21:35 +0530

    Add remaining files and folders

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text>Not Storybook!</Text>
+    </SafeAreaView>
+  );
+};
